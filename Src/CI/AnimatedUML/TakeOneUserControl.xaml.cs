bffc4a95c034ccd1a22ed931c93cd1176b1075ad
AlexPi <alex.pigida@outlook.com>
2021-02-07 16:17:35 -0500

    progress

--- Src/CI/AnimatedUML/TakeOneUserControl.xaml.cs
+++ Src/CI/AnimatedUML/TakeOneUserControl.xaml.cs
@@ -1,28 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AnimatedUML
 {
-  /// <summary>
-  /// Interaction logic for TakeOneUserControl.xaml
-  /// </summary>
   public partial class TakeOneUserControl : UserControl
   {
-    public TakeOneUserControl()
-    {
-      InitializeComponent();
-    }
+    public TakeOneUserControl() => InitializeComponent();
+
+    void onCLose(object sender, RoutedEventArgs e) => App.Current.Shutdown(); // ~~ Close();
   }
 }
