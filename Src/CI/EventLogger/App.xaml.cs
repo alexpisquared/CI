ed86dd5e6777445e0988376904e254827b290550
AlexPi <alex.pigida@outlook.com>
2021-02-11 22:36:05 -0500

    exploring Task Scheduler behavior

---
+++ Src/CI/EventLogger/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EventLogger
+{
+  public partial class App : Application
+  {
+    public const string _textLog = @"C:\temp\EventLog.txt";
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      File.AppendAllText(_textLog, $"\n{DateTime.Now:yyyy-MM-dd HH:mm:ss}  {string.Join(' ', Environment.GetCommandLineArgs())}\n");
+    }
+  }
+}
