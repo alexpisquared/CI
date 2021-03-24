a7c505e01081f56e9066c6f245b88fd08a666ed7
AlexPi <alex.pigida@outlook.com>
2021-03-23 21:30:09 -0400

    harnessed collection view

--- Src/CI/CI.PermissionManager/MainWindow.xaml.cs
+++ Src/CI/CI.PermissionManager/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DB.Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +18,48 @@ using System.Windows.Shapes;
 
 namespace CI.PermissionManager
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
   public partial class MainWindow : Window
   {
+    InventoryContext _db;
+    ICollectionView _cv;
     public MainWindow()
     {
       InitializeComponent();
+
+      _db = new InventoryContext();
+
+      _cv = CollectionViewSource.GetDefaultView(_db.Permissions.Local.ToObservableCollection());
+      CV.Filter = filterPerms;
+      //demo only - wrecks sorting -      CV.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Permission.Name)));
+      CV.SortDescriptions.Add(new SortDescription(nameof(Permission.Name), ListSortDirection.Ascending));
+
+      DataContext = this;
+
+      Loaded += onLoaded;
     }
+    public static readonly DependencyProperty PermissionsFilterProperty = DependencyProperty.Register("PermissionsFilter", typeof(string), typeof(MainWindow), new PropertyMetadata("", propChngdCallback));    public string PermissionsFilter { get { return (string)GetValue(PermissionsFilterProperty); } set { SetValue(PermissionsFilterProperty, value); } }
+    static void propChngdCallback(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MainWindow)d).RefreshView();
+
+    public void RefreshView() => CV.Refresh();
+
+    public ICollectionView CV { get => _cv; }
+
+    bool filterPerms(object obj)
+    {
+      if (obj is Permission permission)
+      {
+        return permission.Name.Contains(PermissionsFilter, StringComparison.InvariantCultureIgnoreCase);
+      }
+            
+
+      return false;
+    }
+
+     void onLoaded(object sender, RoutedEventArgs e)
+    {
+      _db.Permissions.Load();      //await _db.Permissions.LoadAsync();
+      Title = $"{_db.Permissions.Local.Count}";
+    }
+    void onExit(object s, RoutedEventArgs e) => Close();
   }
 }
--- Src/CI/CI.PermissionManager/ReadMe.md
+++ Src/CI/CI.PermissionManager/ReadMe.md
@@ -1,2 +1,4 @@
 ﻿#2021-03-23
 C:\Users\alex.pigida\OneDrive\Documents\SQL Server Management Studio\Permission Management.sql
+
+Filtering, Sorting, and Grouping w/ Collection Views - EASY WPF (.NET CORE)  https://www.youtube.com/watch?v=fBKW-spQboc
