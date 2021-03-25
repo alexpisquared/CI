650fcfe31c3b09a9991ef1e8083b46a598b3953a
AlexPi <alex.pigida@outlook.com>
2021-03-25 17:43:49 -0400

    top-to-bottom hierarchy editor

--- Src/CI/CI.PermissionManager/MainWindow.xaml.cs
+++ Src/CI/CI.PermissionManager/MainWindow.xaml.cs
@@ -1,37 +1,23 @@
 ﻿using DB.Inventory.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CI.PermissionManager
 {
   public partial class MainWindow : Window
   {
-    ICollectionView _cv;
-
+    readonly ICollectionView _cv;
     readonly InventoryContext _context = new();
-    CollectionViewSource categoryViewSource;
+    readonly CollectionViewSource _permissionViewSource;
 
     public MainWindow()
     {
       InitializeComponent();
 
-
-      categoryViewSource = (CollectionViewSource)FindResource(nameof(categoryViewSource));
-
+      _permissionViewSource = (CollectionViewSource)FindResource(nameof(_permissionViewSource));
 
       _cv = CollectionViewSource.GetDefaultView(_context.Permissions.Local.ToObservableCollection());
       CV.Filter = filterPerms;
@@ -42,38 +28,28 @@ namespace CI.PermissionManager
 
       Loaded += onLoaded;
     }
-    public static readonly DependencyProperty PermissionsFilterProperty = DependencyProperty.Register("PermissionsFilter", typeof(string), typeof(MainWindow), new PropertyMetadata("", propChngdCallback)); public string PermissionsFilter { get { return (string)GetValue(PermissionsFilterProperty); } set { SetValue(PermissionsFilterProperty, value); } }
+    public static readonly DependencyProperty PermissionsFilterProperty = DependencyProperty.Register("PermissionsFilter", typeof(string), typeof(MainWindow), new PropertyMetadata("", propChngdCallback)); public string PermissionsFilter { get => (string)GetValue(PermissionsFilterProperty); set => SetValue(PermissionsFilterProperty, value); }
     static void propChngdCallback(DependencyObject d, DependencyPropertyChangedEventArgs e) => ((MainWindow)d).RefreshView();
-
     public void RefreshView() => CV.Refresh();
-
-    public ICollectionView CV { get => _cv; }
-
-    bool filterPerms(object obj)
-    {
-      if (obj is Permission permission)
-      {
-        return permission.Name.Contains(PermissionsFilter, StringComparison.InvariantCultureIgnoreCase);
-      }
-
-
-      return false;
-    }
+    public ICollectionView CV => _cv;
+    bool filterPerms(object obj) => (obj is Permission permission) && permission.Name.Contains(PermissionsFilter, StringComparison.InvariantCultureIgnoreCase);
 
     void onLoaded(object sender, RoutedEventArgs e)
     {
-      _context.Permissions.Load();      //await _context.Permissions.LoadAsync();
-      Title = $"{_context.Permissions.Local.Count}";
+      _context.Applications.Load();           //await _context.Permissions.LoadAsync();
+      _context.Permissions.Load();            //await _context.Permissions.LoadAsync();
+      _context.PermissionAssignments.Load();  //await _context.Permissions.LoadAsync();
+      Title = $"P:{_context.Permissions.Local.Count} pa:{_context.PermissionAssignments.Local.Count}";
 
       //_context.Database.EnsureCreated();
-      categoryViewSource.Source = _context.Permissions.Local.ToObservableCollection();
+      _permissionViewSource.Source = _context.Permissions.Local.ToObservableCollection();
     }
     void onSave(object s, RoutedEventArgs e)
     {
       _context.SaveChanges();
 
-      categoryDataGrid.Items.Refresh();      // this forces the grid to refresh to latest values
-      productsDataGrid.Items.Refresh();
+      _permissionDataGrid.Items.Refresh();      // this forces the grid to refresh to latest values
+      _applicationPermissionsDataGrid.Items.Refresh();
     }
     void onExit(object s, RoutedEventArgs e) => Close();
 
---
+++ Src/CI/CI.PermissionManager/Views/AppPermPAsWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DB.Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CI.PermissionManager.Views
+{
+  public partial class AppPermPAsWindow : Window
+  {
+    readonly InventoryContext _context = new();
+    readonly CollectionViewSource _applicationViewSource;
+
+    public AppPermPAsWindow()
+    {
+      InitializeComponent();
+
+      _applicationViewSource = (CollectionViewSource)FindResource(nameof(_applicationViewSource));
+
+      DataContext = this;
+
+      Loaded += onLoaded;
+    }
+    void onLoaded(object sender, RoutedEventArgs e)
+    {
+      _context.Applications.Load();           //await _context.Permissions.LoadAsync();
+      _context.Permissions.Load();            //await _context.Permissions.LoadAsync();
+      _context.PermissionAssignments.Load();  //await _context.Permissions.LoadAsync();
+      Title = $"A:{_context.Applications.Local.Count} ◄ P:{_context.Permissions.Local.Count} ◄ pa:{_context.PermissionAssignments.Local.Count}";
+
+      _applicationViewSource.Source = _context.Applications.Local.ToObservableCollection();
+    }
+    void onSave(object s, RoutedEventArgs e)
+    {
+      _context.SaveChanges();
+
+      _applicationDataGrid.Items.Refresh();      // this forces the grid to refresh to latest values
+      _applicationPermissionsDataGrid.Items.Refresh();
+      _applicationPermissionPermissionAssignmentsDataGrid.Items.Refresh();
+    }
+    void onExit(object s, RoutedEventArgs e) => Close();
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+      _context.Dispose();
+      base.OnClosing(e);
+    }
+  }
+}
