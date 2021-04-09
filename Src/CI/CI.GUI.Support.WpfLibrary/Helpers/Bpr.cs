e1821afdc6aa8a5263086d7e3888d47eeed0beab
AlexPi <alex.pigida@outlook.com>
2021-04-09 17:24:28 -0400

    Update Bpr.cs

--- Src/CI/CI.GUI.Support.WpfLibrary/Helpers/Bpr.cs
+++ Src/CI/CI.GUI.Support.WpfLibrary/Helpers/Bpr.cs
@@ -1,8 +1,13 @@
-﻿namespace CI.GUI.Support.WpfLibrary.Helpers
+﻿using System;
+
+namespace CI.GUI.Support.WpfLibrary.Helpers
 {
   public class Bpr
   {
-    public static void ErrorFaF() => NativeMethods.Beep(4444, 333);
+    const int _a= 7000, _b = 8000, _msA = 100, _msB = 200;
+    public static void ErrorFaF() => NativeMethods.Beep(_a, 333);
     public static void Beep(int freq, int dur) => NativeMethods.Beep(freq, dur);
+    public static void Start() { Beep(_a, _msA); Beep(_b, _msB); }
+    public static void Finish() { Beep(_b, _msB); Beep(_a, _msA); }
   }
 }
