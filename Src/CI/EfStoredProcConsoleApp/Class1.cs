6992708a40e63bb466c118b3f2a2f66dea5bac62
AlexPi <alex.pigida@outlook.com>
2021-04-16 22:55:10 -0400

    Update Class1.cs

--- Src/CI/EfStoredProcConsoleApp/Class1.cs
+++ Src/CI/EfStoredProcConsoleApp/Class1.cs
@@ -8,9 +8,14 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 /// <summary>
-/// FromSql
+/// 
+/// MS Docs
 /// https://docs.microsoft.com/en-us/ef/core/querying/raw-sql 
 /// https://github.com/dotnet/EntityFramework.Docs/tree/main/samples/core/Querying/RawSQL
+/// 
+/// Cool sites to check:
+/// https://www.michalbialecki.com/2020/09/14/executing-raw-sql-with-entity-framework-core-5/
+/// https://www.learnentityframeworkcore.com/raw-sql
 ///  
 /// </summary>
 
@@ -27,15 +32,26 @@ namespace EfStoredProcConsoleApp
     }
     public void ExecuteSqlCommand() // https://www.entityframeworktutorial.net/efcore/working-with-stored-procedure-in-ef-core.aspx
     {
-/*CREATE PROCEDURE CreateStudent
-    @FirstName Varchar(50),
-    @LastName Varchar(50)
-AS
-BEGIN
-    SET NOCOUNT ON;
-    Insert into Students( [FirstName] ,[LastName] ) Values (@FirstName, @LastName)
-END*/
+      /*CREATE PROCEDURE CreateStudent
+          @FirstName Varchar(50),
+          @LastName Varchar(50)
+      AS
+      BEGIN
+          SET NOCOUNT ON;
+          Insert into Students( [FirstName] ,[LastName] ) Values (@FirstName, @LastName)
+      END*/
       context.Database.ExecuteSqlRaw("CreateStudents @p0, @p1", parameters: new[] { "Bill", "Gates" }); // ~ var rowsAffected = context.Database.ExecuteSqlCommand("Update Students set FirstName = 'Bill' where StudentId = 1;");
+    }
+    public void Projection()        // https://www.learnentityframeworkcore.com/raw-sql
+    {
+      var books = context.Books
+          .FromSqlRaw("SELECT * FROM Books")
+          .Select(b => new
+          {
+            BookId___ = b.BookId,
+            ShortName = b.ShortName,
+            CapTrans_ = b.CapTrans
+          }).ToList();
     }
   }
 }
