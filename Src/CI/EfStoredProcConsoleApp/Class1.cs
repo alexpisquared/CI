bf9ce13a240a4cf03e8cc316a9f4e2afe83d79ff
AlexPi <alex.pigida@outlook.com>
2021-04-17 14:54:26 -0400

    Update Class1.cs

--- Src/CI/EfStoredProcConsoleApp/Class1.cs
+++ Src/CI/EfStoredProcConsoleApp/Class1.cs
@@ -1,11 +1,8 @@
 ﻿using DB.Inventory.Models;
 using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 /// MS Docs
 /// https://docs.microsoft.com/en-us/ef/core/querying/raw-sql 
@@ -25,34 +22,30 @@ namespace EfStoredProcConsoleApp
 
     public void FromSql()           // https://www.entityframeworktutorial.net/efcore/working-with-stored-procedure-in-ef-core.aspx
     {
+      // USE [Inventory] GO
+      //DECLARE	@return_value int      EXEC	@return_value = [dbo].[usp_Report_AllCashByGroup]		@pGroup_ID = 2,		@pDateType = N'2',		@pStartDateInt = 2,		@pEndDateInt = 2,		@pGroupName = N'2'          SELECT	'Return Value' = @return_value
+      //CREATE PROCEDURE [dbo].[usp_Report_AllCashByGroup]  @pGroup_ID int = -1,  @pDateType char(1) = 'T', /*T = TradeDate S = SettlmntDate* /  @pStartDateInt int = 0, @pEndDateInt int = 0, @pGroupName varchar(50)        AS      select * from BookReport_view where  Book_ID in (select Book_ID from GroupMember where Group_ID = @pGroup_ID)
       var param = new SqlParameter("@FirstName", "Bill"); // new SqlParameter() { ParameterName = "@FirstName", SqlDbType =  System.Data.SqlDbType.VarChar, Direction = System.Data.ParameterDirection.Input, Size = 50, Value = "Bill" };
-
       var parms = new[] {
-        new SqlParameter("@pGroup_ID", 2),   
+        new SqlParameter("@pGroup_ID", 2),
         new SqlParameter("@pDateType", "2"),
         new SqlParameter("@pStartDateInt", "2"),
         new SqlParameter("@pEndDateInt", 2),
         new SqlParameter("@pGroupName", "2")        };
 
       var rows = context.BookReportViews.FromSqlRaw("usp_Report_AllCashByGroup @pGroup_ID, @pDateType, @pStartDateInt, @pEndDateInt, @pGroupName", parms).ToList();
-      Console.WriteLine($"** {rows.Count}");
+      Console.WriteLine($"** {rows.Count}  rows returned");
     }
     public void ExecuteSqlCommand() // https://www.entityframeworktutorial.net/efcore/working-with-stored-procedure-in-ef-core.aspx
     {
-      /*CREATE PROCEDURE CreateStudent
-          @FirstName Varchar(50),
-          @LastName Varchar(50)
-      AS
-      BEGIN
-          SET NOCOUNT ON;
-          Insert into Students( [FirstName] ,[LastName] ) Values (@FirstName, @LastName)
-      END*/
-      context.Database.ExecuteSqlRaw("CreateStudents @p0, @p1", parameters: new[] { "Bill", "Gates" }); // ~ var rowsAffected = context.Database.ExecuteSqlCommand("Update Students set FirstName = 'Bill' where StudentId = 1;");
+      var rv = context.Database.ExecuteSqlRaw("usp_AddAccountGroupSymbol @p0, @p1", new[] {
+        new SqlParameter("@p0", 2),
+        new SqlParameter("@p1", "AGF")        }); // ~ var rowsAffected = context.Database.ExecuteSqlCommand("Update Students set FirstName = 'Bill' where StudentId = 1;");
+      Console.WriteLine($"** {rv}  -1 0 1 ");
     }
     public void Projection()        // https://www.learnentityframeworkcore.com/raw-sql
     {
-      var rows = context.Users
-          .FromSqlRaw("SELECT * FROM Users")
+      var rows = context.Users.FromSqlRaw("SELECT * FROM Users")
           .Select(b => new
           {
             UserId___ = b.UserId,
@@ -61,24 +54,7 @@ namespace EfStoredProcConsoleApp
             Status___ = b.Status
           }).ToList();
 
-      Console.WriteLine($"** {rows.Count}");
+      Console.WriteLine($"** {rows.Count}  rows returned");
     }
   }
 }
-/*
-USE [Inventory26]
-GO
-DECLARE	@return_value int
-EXEC	@return_value = [dbo].[usp_Report_AllCashByGroup]		@pGroup_ID = 2,		@pDateType = N'2',		@pStartDateInt = 2,		@pEndDateInt = 2,		@pGroupName = N'2'
-SELECT	'Return Value' = @return_value
-GO
-
-CREATE PROCEDURE [dbo].[usp_Report_AllCashByGroup]
-  @pGroup_ID int = -1,
-  @pDateType char(1) = 'T', -- T = TradeDate S = SettlmntDate
-  @pStartDateInt int = 0,
-  @pEndDateInt int = 0,
-  @pGroupName varchar(50)
-AS
-  select * from BookReport_view where  Book_ID in (select Book_ID from GroupMember where Group_ID = @pGroup_ID)
-*/
