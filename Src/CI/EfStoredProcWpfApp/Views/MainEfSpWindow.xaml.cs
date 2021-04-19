943a356ec69b6601ed21642778c6d294965453d9
AlexPi <alex.pigida@outlook.com>
2021-04-19 16:23:33 -0400

    initial visuals

--- Src/CI/EfStoredProcWpfApp/Views/MainEfSpWindow.xaml.cs
+++ Src/CI/EfStoredProcWpfApp/Views/MainEfSpWindow.xaml.cs
@@ -20,5 +20,16 @@ namespace EfStoredProcWpfApp.Views
     {
       InitializeComponent();
     }
+
+    public static readonly DependencyProperty BlurProperty = DependencyProperty.Register("Blur", typeof(double), typeof(MainEfSpWindow), new PropertyMetadata(.0)); public double Blur { get => (double)GetValue(BlurProperty); set => SetValue(BlurProperty, value); }
+
+    void onWindowRestoree(object s, RoutedEventArgs e) { wr.Visibility = Visibility.Collapsed; wm.Visibility = Visibility.Visible; WindowState = WindowState.Normal; }
+    void onWindowMaximize(object s, RoutedEventArgs e) { wm.Visibility = Visibility.Collapsed; wr.Visibility = Visibility.Visible; WindowState = WindowState.Maximized; }
+    async void onSave(object s, RoutedEventArgs e) { }
+    async void onAudio(object s, RoutedEventArgs e) { }
+    async void onFlush(object s, RoutedEventArgs e) { }
+    async void onAddMe(object s, RoutedEventArgs e) { }
+    async void cbxSrvr_SelectionChanged(object s, RoutedEventArgs e) { }
+    async void onSettings(object s, RoutedEventArgs e) { }
   }
 }
