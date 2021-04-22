a9edb8c78d6ced030db0932c604be322e2152247
AlexPi <alex.pigida@outlook.com>
2021-04-22 13:19:56 -0400

    Cleaner look. More func-ty.

--- Src/CI/EfStoredProcWpfApp/Views/SpDemoTableView.xaml.cs
+++ Src/CI/EfStoredProcWpfApp/Views/SpDemoTableView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DB.Inventory.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,21 +16,35 @@ namespace EfStoredProcWpfApp.Views
 
     async Task FromSql()           // https://www.entityframeworktutorial.net/efcore/working-with-stored-procedure-in-ef-core.aspx
     {
-      // USE [Inventory] GO
-      //DECLARE	@return_value int      EXEC	@return_value = [dbo].[usp_Report_AllCashByGroup]		@pGroup_ID = 2,		@pDateType = N'2',		@pStartDateInt = 2,		@pEndDateInt = 2,		@pGroupName = N'2'          SELECT	'Return Value' = @return_value
-      //CREATE PROCEDURE [dbo].[usp_Report_AllCashByGroup]  @pGroup_ID int = -1,  @pDateType char(1) = 'T', /*T = TradeDate S = SettlmntDate* /  @pStartDateInt int = 0, @pEndDateInt int = 0, @pGroupName varchar(50)        AS      select * from BookReport_view where  Book_ID in (select Book_ID from GroupMember where Group_ID = @pGroup_ID)
-      var param = new SqlParameter("@FirstName", "Bill"); // new SqlParameter() { ParameterName = "@FirstName", SqlDbType =  System.Data.SqlDbType.VarChar, Direction = System.Data.ParameterDirection.Input, Size = 50, Value = "Bill" };
-      var parms = new[] {
+      try
+      {
+        ctrlPanel.IsEnabled = false;
+
+        // USE [Inventory] GO
+        //DECLARE	@return_value int      EXEC	@return_value = [dbo].[usp_Report_AllCashByGroup]		@pGroup_ID = 2,		@pDateType = N'2',		@pStartDateInt = 2,		@pEndDateInt = 2,		@pGroupName = N'2'          SELECT	'Return Value' = @return_value
+        //CREATE PROCEDURE [dbo].[usp_Report_AllCashByGroup]  @pGroup_ID int = -1,  @pDateType char(1) = 'T', /*T = TradeDate S = SettlmntDate* /  @pStartDateInt int = 0, @pEndDateInt int = 0, @pGroupName varchar(50)        AS      select * from BookReport_view where  Book_ID in (select Book_ID from GroupMember where Group_ID = @pGroup_ID)
+        var param = new SqlParameter("@FirstName", "Bill"); // new SqlParameter() { ParameterName = "@FirstName", SqlDbType =  System.Data.SqlDbType.VarChar, Direction = System.Data.ParameterDirection.Input, Size = 50, Value = "Bill" };
+        var parms = new[] {
         new SqlParameter("@pGroup_ID",      /**/ t1.Text),
         new SqlParameter("@pDateType",      /**/ t2.Text),
-        new SqlParameter("@pStartDateInt",  /**/ t3.Text),
-        new SqlParameter("@pEndDateInt",    /**/ t4.Text),
-        new SqlParameter("@pGroupName",     /**/ t5.Text)        };
+        new SqlParameter("@pStartDateInt",  /**/ (int)DateTime.Parse(t3.Text).ToOADate()),
+        new SqlParameter("@pEndDateInt",    /**/ (int)DateTime.Parse(t4.Text).ToOADate()),
+        new SqlParameter("@pGroupName",     /**/ t5.Text)};
 
-      var rows = await context.BookReportViews.FromSqlRaw("usp_Report_AllCashByGroup @pGroup_ID, @pDateType, @pStartDateInt, @pEndDateInt, @pGroupName", parms).ToListAsync();
-      Debug.WriteLine($"** {rows.Count}  rows returned");
+        var rows = await context.BookReportViews.FromSqlRaw("usp_Report_AllCashByGroup @pGroup_ID, @pDateType, @pStartDateInt, @pEndDateInt, @pGroupName", parms).ToListAsync();
+        Debug.WriteLine($"** {rows.Count}  rows returned");
 
-      dg1.ItemsSource = rows;
+        dg1.ItemsSource = rows;
+        tbkError.Text = $"{rows.Count} rows found";
+      }
+      catch (System.Exception ex)
+      {
+        tbkError.Text = ex.Message;
+      }
+      finally
+      {
+        ctrlPanel.IsEnabled = true;
+      }
     }
 
     async void onGo(object sender, RoutedEventArgs e) { await FromSql(); ; }
