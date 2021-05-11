f4d2f45032b967edd92b1e45adbf9afcfa294d40
AlexPi <alex.pigida@outlook.com>
2021-05-11 15:04:59 -0400

    selector model++

--- Src/CI/CI.DS.ViewModel/Commands/UpdateViewCommand.cs
+++ Src/CI/CI.DS.ViewModel/Commands/UpdateViewCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Windows.Input;
 
 namespace CI.DS.ViewModel.Commands
@@ -7,13 +8,14 @@ namespace CI.DS.ViewModel.Commands
   {
     readonly MainVM _mainVM;
 
-    public UpdateViewCommand(MainVM mainVM) => _mainVM = mainVM;
+    public UpdateViewCommand(ObservableValidator mainVM) => _mainVM = (MainVM)mainVM;
+
 
     public event EventHandler? CanExecuteChanged;
-
     public bool CanExecute(object? parameter) => true;
     public void Execute(object? parameter) => _mainVM.SelectedVM = parameter switch
     {
+      "Usps" => new DbProcessSelectorVM(_mainVM.Logger, _mainVM.Config),
       "Demo" => new DemoVM(_mainVM.Logger, _mainVM.Config),
       "Acbg" => new AllCashByGroupVM(_mainVM.Logger, _mainVM.Config),
       _ => throw new MissingFieldException(parameter?.ToString()),
