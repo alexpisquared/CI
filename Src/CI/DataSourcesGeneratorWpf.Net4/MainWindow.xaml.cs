c57bb7f54ac29550b385c57abde0d0152fa5c88f
AlexPi <alex.pigida@outlook.com>
2021-05-13 15:13:52 -0400

    prep for better OrderHistryCID popup view

---
+++ Src/CI/DataSourcesGeneratorWpf.Net4/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace DataSourcesGeneratorWpf.Net4
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow() { InitializeComponent(); }
+
+    void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      TradeDataDataSet tradeDataDataSet = ((TradeDataDataSet)(FindResource("tradeDataDataSet")));
+
+      TradeDataDataSetTableAdapters.OrderHistoryCIDTableAdapter tradeDataDataSetOrderHistoryCIDTableAdapter = new DataSourcesGeneratorWpf.Net4.TradeDataDataSetTableAdapters.OrderHistoryCIDTableAdapter();
+      tradeDataDataSetOrderHistoryCIDTableAdapter.Fill(tradeDataDataSet.OrderHistoryCID);
+      System.Windows.Data.CollectionViewSource orderHistoryCIDViewSource = ((System.Windows.Data.CollectionViewSource)(FindResource("orderHistoryCIDViewSource")));
+      orderHistoryCIDViewSource.View.MoveCurrentToFirst();
+    }
+
+    void onClose(object sender, RoutedEventArgs e) { Close(); }
+  }
+}
