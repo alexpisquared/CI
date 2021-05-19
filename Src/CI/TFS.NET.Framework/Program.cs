141b38784fc8e38be97e6d4ec8315f16c6967626
AlexPi <alex.pigida@outlook.com>
2021-05-19 16:59:17 -0400

    Update Program.cs

--- Src/CI/TFS.NET.Framework/Program.cs
+++ Src/CI/TFS.NET.Framework/Program.cs
@@ -9,112 +9,123 @@ using System.Text;
 
 namespace TFS
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      var filePatterns = args[0].Split(new char[] { '`' }, StringSplitOptions.RemoveEmptyEntries); // new[] { "*.jar", "*.cs", "*.cpp" };//"*.?", "*.??", "*.???", "*.????", "*.?????", "*.??????" };// "*.cs", "*.xml", "*.config", "*.asp", "*.aspx", "*.js", "*.h", "*.cpp", "*.vb", "*.asax", "*.ashx", "*.asmx", "*.ascx", "*.master", "*.svc", "*.jar" }; //file extensions
+      var textPatterns = args.Length < 2 ? null : args[1].Split(new char[] { '`' }, StringSplitOptions.RemoveEmptyEntries);
+
+      Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write($"  Searching for  ");
+      Console.ForegroundColor = ConsoleColor.Cyan; Console.Write($"{(args.Length < 2 ? "" : args[1])}");
+      Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write($"  in  ");
+      Console.ForegroundColor = ConsoleColor.Cyan; Console.Write($"{args[0]} \n");
+
+      var now = DateTime.Now;
+      var fnm = (args.Length < 2 ? "fnm" : args[1]).Replace(":", "-").Replace("|", " ").Replace("?", "-");
+      var outputF = $@"C:\temp\TFS.Search\{fnm} - {now:HHmm} - Filenames Only.txt";
+      var details = $@"C:\temp\TFS.Search\{fnm} - {now:HHmm} - Files & Lines.txt";
+      var headerL = $"Times  Checked-in  Filename       Searching for  '{(args.Length < 2 ? "" : args[1])}'  in  '{args[0]}'    {Environment.NewLine}";
+
+      //Process.Start("Explorer.exe", @"D:\temp\TFS.Search\");
+
+      try
+      {
+        var tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri("http://dev-tfs1.pariotech.com:8080/tfs/defaultcollection"));
+        tfs.EnsureAuthenticated();
+
+        var versionControl = tfs.GetService<VersionControlServer>();
+
+        File.AppendAllText(outputF, headerL);
+        File.AppendAllText(details, headerL);
+
+        var allProjs = versionControl.GetAllTeamProjects(true);
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        //Console.WriteLine($"  {allProjs.Count():N0} team projects: {string.Join<TeamProject>(", ", allProjs)}");
+        Console.Write($"  {allProjs.Count():N0} team projects:  ");
+
+        var i = 0;
+        var f = 0;
+        foreach (var teamProj in allProjs)
         {
-            string[]
-              textPatterns = args[0].Split(new char[] { '`' }, StringSplitOptions.RemoveEmptyEntries),
-              filePatterns = args[1].Split(new char[] { '`' }, StringSplitOptions.RemoveEmptyEntries);// new[] { "*.jar", "*.cs", "*.cpp" };//"*.?", "*.??", "*.???", "*.????", "*.?????", "*.??????" };// "*.cs", "*.xml", "*.config", "*.asp", "*.aspx", "*.js", "*.h", "*.cpp", "*.vb", "*.asax", "*.ashx", "*.asmx", "*.ascx", "*.master", "*.svc", "*.jar" }; //file extensions
+          Console.ForegroundColor = ConsoleColor.Gray;
+          Console.WriteLine($"{teamProj.Name}");
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write($"  Searching for  ");
-            Console.ForegroundColor = ConsoleColor.Cyan; Console.Write($"{args[0]}");
-            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write($"  in  ");
-            Console.ForegroundColor = ConsoleColor.Cyan; Console.Write($"{args[1]} \n");
-
-            var now = DateTime.Now;
-            var fnm = args[0].Replace(":", "-").Replace("|", " ").Replace("?", "-");
-            var outputF = $@"C:\temp\TFS.Search\{fnm} - {now:HHmm} - Filenames Only.txt";
-            var details = $@"C:\temp\TFS.Search\{fnm} - {now:HHmm} - Files & Lines.txt";
-            var headerL = $"Times  Checked-in  Filename       Searching for  '{args[0]}'  in  '{args[1]}'    {Environment.NewLine}";
-
-            Process.Start("Explorer.exe", @"C:\temp\TFS.Search\");
-
-            try
+          foreach (var filePattern in filePatterns)
+          {
+            var files = versionControl.GetItems(teamProj.ServerItem + "/" + filePattern, RecursionType.Full).Items.Where(r => r.ItemType == ItemType.File && !r.ServerItem.Contains("_ReSharper")); //skipping resharper stuff
+            Console.WriteLine($"  {++f} / {filePatterns.Length} - {filePattern} - {files.Count():N0} files:");
+            foreach (var file in files)
             {
-                var tfs = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri("http://dev-tfs1.pariotech.com:8080/tfs/defaultcollection"));
-                tfs.EnsureAuthenticated();
+              if ((++i) % 11 == 0)
+                Console.Write($"{i,8:N0} / {files.Count():N0} {file.ServerItem}                                                                                                          \r");
 
-                var versionControl = tfs.GetService<VersionControlServer>();
-
-                File.AppendAllText(outputF, headerL);
-                File.AppendAllText(details, headerL);
-
-                var allProjs = versionControl.GetAllTeamProjects(true);
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                //Console.WriteLine($"  {allProjs.Count():N0} team projects: {string.Join<TeamProject>(", ", allProjs)}");
-                Console.Write($"  {allProjs.Count():N0} team projects:  ");
-
-                var i = 0;
-                var f = 0;
-                foreach (var teamProj in allProjs)
+              if (textPatterns == null)
+              {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                var header = $"{"",5}  {file.CheckinDate:yyyy-MM-dd}  {file.ServerItem}   {Environment.NewLine}";
+                Console.Write(header);
+                File.AppendAllText(outputF, header);
+              }
+              else
+              {
+                var lines = SearchInFile(file, textPatterns);
+                if (lines.Count > 0)
                 {
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.WriteLine($"{teamProj.Name}");
+                  Console.ForegroundColor = ConsoleColor.DarkYellow;
+                  var header = $"{lines.Count,5}  {file.CheckinDate:yyyy-MM-dd}  {file.ServerItem}   {Environment.NewLine}";
+                  Console.Write(header);
+                  File.AppendAllText(outputF, header);
+                  File.AppendAllText(details, header);
 
-                    foreach (var filePattern in filePatterns)
-                    {
-                        var files = versionControl.GetItems(teamProj.ServerItem + "/" + filePattern, RecursionType.Full).Items.Where(r => r.ItemType == ItemType.File && !r.ServerItem.Contains("_ReSharper")); //skipping resharper stuff
-                        Console.WriteLine($"  {++f} / {filePatterns.Length} - {filePattern} - {files.Count():N0} files:");
-                        foreach (var item in files)
-                        {
-                            if ((++i) % 11 == 0)
-                                Console.Write($"{i,8:N0} / {files.Count():N0} {item.ServerItem}                                                                                                          \r");
+                  foreach (var line in lines)
+                  {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"  {line}");
+                    File.AppendAllText(details, $"  {line}{Environment.NewLine}");
+                  }
 
-                            var lines = SearchInFile(item, textPatterns);
-                            if (lines.Count > 0)
-                            {
-                                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                                var header = $"{lines.Count,5}  {item.CheckinDate:yyyy-MM-dd}  {item.ServerItem}   {Environment.NewLine}";
-                                Console.Write(header);
-                                File.AppendAllText(outputF, header);
-                                File.AppendAllText(details, header);
-
-                                foreach (var line in lines)
-                                {
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.WriteLine($"  {line}");
-                                    File.AppendAllText(details, $"  {line}{Environment.NewLine}");
-                                }
-
-                                Console.ForegroundColor = ConsoleColor.Gray;
-                            }
-                        }
-                    }
+                  Console.ForegroundColor = ConsoleColor.Gray;
                 }
+              }
             }
-            catch (Exception ex) { Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine($"{ex}"); Console.ForegroundColor = ConsoleColor.Gray; }
-            finally
-            {
-                File.AppendAllText(details, $"\n from - to = took:  {now:HH:mm} - {DateTime.Now:HH:mm} = {(DateTime.Now - now).TotalMinutes:N1} min");
-            }
-
-            //Console.ForegroundColor = ConsoleColor.DarkGreen;      Console.WriteLine("======== Press any key ");      Console.ResetColor();      Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"  {f} / {filePatterns.Length} - {filePattern} - {files.Count():N0} files.");
+          }
         }
+      }
+      catch (Exception ex) { Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine($"{ex}"); Console.ForegroundColor = ConsoleColor.Gray; }
+      finally
+      {
+        File.AppendAllText(details, $"\n from - to = took:  {now:HH:mm} - {DateTime.Now:HH:mm} = {(DateTime.Now - now).TotalMinutes:N1} min");
+      }
 
-        static List<string> SearchInFile(Item file, string[] textPatterns)
-        {
-            var result = new List<string>();
-
-            try
-            {
-                var stream = new StreamReader(file.DownloadFile(), Encoding.Default);
-
-                var line = stream.ReadLine();
-                var lineIndex = 0;
-
-                while (!stream.EndOfStream)
-                {
-                    if (textPatterns.Any(p => line.IndexOf(p, StringComparison.OrdinalIgnoreCase) >= 0))
-                        result.Add($"{lineIndex,6}: {line.Trim()}");
-
-                    line = stream.ReadLine();
-                    lineIndex++;
-                }
-            }
-            catch (Exception ex) { Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine($"{ex}"); Console.ForegroundColor = ConsoleColor.Gray; }
-            return result;
-        }
+      //Console.ForegroundColor = ConsoleColor.DarkGreen;      Console.WriteLine("======== Press any key ");      Console.ResetColor();      Console.ReadKey();
     }
+
+    static List<string> SearchInFile(Item file, string[] textPatterns)
+    {
+      var result = new List<string>();
+
+      try
+      {
+        var stream = new StreamReader(file.DownloadFile(), Encoding.Default);
+
+        var line = stream.ReadLine();
+        var lineIndex = 0;
+
+        while (!stream.EndOfStream)
+        {
+          if (textPatterns.Any(p => line.IndexOf(p, StringComparison.OrdinalIgnoreCase) >= 0))
+            result.Add($"{lineIndex,6}: {line.Trim()}");
+
+          line = stream.ReadLine();
+          lineIndex++;
+        }
+      }
+      catch (Exception ex) { Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine($"{ex}"); Console.ForegroundColor = ConsoleColor.Gray; }
+      return result;
+    }
+  }
 }
 
