6601102af65af04b0a3bddfa30a7dbf0e21bb388
AlexPi <alex.pigida@outlook.com>
2021-07-17 22:10:51 -0400

    ++

--- Src/CI/DB.Inventory.Dbo/Models/InventoryContext.cs
+++ Src/CI/DB.Inventory.Dbo/Models/InventoryContext.cs
@@ -199,8 +199,7 @@ namespace DB.Inventory.Dbo.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=mtDEVsqldb;Database=Inventory;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(_connectoinString);
             }
         }
 
