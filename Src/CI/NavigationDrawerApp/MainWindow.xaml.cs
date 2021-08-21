82f8020781d1e43ba5f176629a56e47a1d3d004a
AlexPi <alex.pigida@outlook.com>
2021-08-21 18:23:46 -0400

    NavigationDrawer

---
+++ Src/CI/NavigationDrawerApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavigationDrawerApp
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      MouseLeftButtonDown += (s, e) => { if (e.LeftButton == MouseButtonState.Pressed) DragMove(); }; 
+      KeyDown += (s, e) => { if (e.Key == Key.Escape) { Close(); System.Diagnostics.Trace.WriteLine($"{DateTime.Now:yy.MM.dd HH:mm:ss.f} => ::>Application.Current.Shutdown();n"); Application.Current.Shutdown(); } };
+    }
+
+    void Button_Click(object sender, RoutedEventArgs e) { Close(); App.Current.Shutdown(); }
+  }
+}
---
+++ Src/CI/NavigationDrawerControl/HamburgerMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace NavigationDrawerControl
+{
+  public class HamburgerMenu : Control
+  {
+    public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register("IsOpen", typeof(bool), typeof(HamburgerMenu), new PropertyMetadata(false, OnIsOpenPrepertyChanged)); public bool IsOpen { get => (bool)GetValue(IsOpenProperty); set => SetValue(IsOpenProperty, value); }
+    public static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(FrameworkElement), typeof(HamburgerMenu), new PropertyMetadata(null)); public FrameworkElement Content { get => (FrameworkElement)GetValue(ContentProperty); set => SetValue(ContentProperty, value); }
+    public static readonly DependencyProperty OpenCloseDurationProperty = DependencyProperty.Register("OpenCloseDuration", typeof(Duration), typeof(HamburgerMenu), new PropertyMetadata(Duration.Automatic)); public Duration OpenCloseDuration { get => (Duration)GetValue(OpenCloseDurationProperty); set => SetValue(OpenCloseDurationProperty, value); }
+
+    static HamburgerMenu() => DefaultStyleKeyProperty.OverrideMetadata(typeof(HamburgerMenu), new FrameworkPropertyMetadata(typeof(HamburgerMenu)));
+    public HamburgerMenu()
+    {
+      Width = 0;
+    }
+
+    static void OnIsOpenPrepertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is HamburgerMenu hamburgerMenu)
+      {
+        hamburgerMenu.OnIsOpenPrepertyChanged();
+      }
+    }
+
+    private void OnIsOpenPrepertyChanged()
+    {
+      if (IsOpen)
+      {
+        OpenMenuAnimated();
+      }
+      else
+      {
+        CloseMenuAnimated();
+      }
+    }
+
+    void OpenMenuAnimated()
+    {
+      Content.Measure(new Size(MaxWidth, MaxHeight)); 
+      BeginAnimation(WidthProperty, new DoubleAnimation(toValue: Content.DesiredSize.Width, OpenCloseDuration));
+    }
+
+    void CloseMenuAnimated() => BeginAnimation(WidthProperty, new DoubleAnimation(toValue: 0, OpenCloseDuration));
+  }
+}
