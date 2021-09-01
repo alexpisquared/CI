e852341e85431727aac82385e9c7f385e42c8cd5
AlexPi <alex.pigida@outlook.com>
2021-09-01 18:44:48 -0400

    pau

--- Src/CI/WinTiler/App.xaml.cs
+++ Src/CI/WinTiler/App.xaml.cs
@@ -22,7 +22,8 @@ namespace WinTiler
       Started = DateTime.Now;
       _config = ConfigHelper.AutoInitConfig(@"{{
   ""WhereAmI"": "" ??\\PermMgrClient\\appsettings.CI.PM.json  DFLT"",
-  ""LogFolder"": ""\\\\bbsfile01\\Public\\Dev\\AlexPi\\Misc\\Logs\\WinTiler.DFLT..txt"",
+  ""LogFolder"": ""C:\\temp\\Logs\\WinTiler.DFLT..txt"",
+  ""LogFolde_"": ""\\\\bbsfile01\\Public\\Dev\\AlexPi\\Misc\\Logs\\WinTiler.DFLT..txt"",
   ""ServerList"": "".\\sqlexpress mtDEVsqldb mtUATsqldb mtPRDsqldb"",
   ""SqlConStr"": ""Server={{0}};Database=Inventory;Trusted_Connection=True;"",
   ""AppSettings"": {{
