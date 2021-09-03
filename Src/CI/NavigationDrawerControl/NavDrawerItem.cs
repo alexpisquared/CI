a0472ae03622f52480908814022ae92d071b2e2c
AlexPi <alex.pigida@outlook.com>
2021-09-03 16:33:02 -0400

    ++

---
+++ Src/CI/NavigationDrawerControl/NavDrawerItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NavigationDrawerControl
+{
+  public class NavDrawerItem : RadioButton
+  {
+    static NavDrawerItem() => DefaultStyleKeyProperty.OverrideMetadata(typeof(NavDrawerItem), new FrameworkPropertyMetadata(typeof(NavDrawerItem)));
+  }
+}
--- Src/CI/NavigationDrawerControl/ReadMe.md
+++ Src/CI/NavigationDrawerControl/ReadMe.md
@@ -1,2 +1,2 @@
 ﻿#2021-08-21
-Hamburger Menu/Navigation Drawer - WPF UI COMPONENT WORKSHOP          https://youtu.be/mjYXnlufyI8?t=7
+Inspired by          Navigation Drawer - WPF UI COMPONENT WORKSHOP          https://youtu.be/mjYXnlufyI8?t=7
