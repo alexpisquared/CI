4fc7019647540d54ddaa6ea1becdd5c2b3cf7de7
Alex Pigida <apigida@ci.com>
2022-06-06 09:15:26 -0400

    poc app.config solution

---
+++ Src/CI/AppSets2UserPref/BuiltInApproachWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace BuiltInApproachWpfApp;
+
+public partial class MainWindow : Window
+{
+  public MainWindow()
+  {
+    InitializeComponent();
+
+    tbk0.Text = $"\n Brand: \t{Properties.Settings.Default.BrandColor} \n\n My:   \t{Properties.Settings.Default.MyColor} \n\n last\t{Properties.Settings.Default.LastRan}\r\n                                    + {DateTime.Now - Properties.Settings.Default.LastRan:mm\\:ss}\r\n now \t{DateTime.Now}\r\n\r\n";
+
+    Properties.Settings.Default.Setting2 =
+    Properties.Settings.Default.MyColor = DateTime.Now.ToString();
+    Properties.Settings.Default.LastRan = DateTime.Now;
+    Properties.Settings.Default.Save();
+  }
+
+  void OnClose(object sender, RoutedEventArgs e) => Close();
+}
