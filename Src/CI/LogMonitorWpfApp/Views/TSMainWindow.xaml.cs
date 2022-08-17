0710e736b90d4ac1cb5447c2e7f062cbab033025
Alex Pigida <apigida@ci.com>
2022-08-17 14:10:16 -0400

    pau

--- Src/CI/LogMonitorWpfApp/Views/TSMainWindow.xaml.cs
+++ Src/CI/LogMonitorWpfApp/Views/TSMainWindow.xaml.cs
@@ -50,7 +50,7 @@ public partial class TSMainWindow : Window
   async void OnLoaded(object s, RoutedEventArgs e)
   {
     dg1.ItemsSource = _userSettingsAndStateOfFS.FileDataList;
-    Title = $"No events since  {DateTime.Now:HH:mm:ss}  -  {VersionHelper.CurVerStr0Md}";
+    Title = $"{DateTime.Now:HH:mm}  No events since  ..  {VersionHelper.CurVerStr0Md}";
     StartWatch();
     await StartPeriodicChecker();
   }
@@ -86,7 +86,7 @@ public partial class TSMainWindow : Window
     {
       RemoveDeleteds();
 
-      Title = $"OnResetW on {DateTime.Now:HH:mm:ss}  -  {VersionHelper.CurVerStr0Md}";
+      Title = $"{DateTime.Now:HH:mm}  OnResetW on  ..  {VersionHelper.CurVerStr0Md}";
     }
     catch (Exception ex) { _ = MessageBox.Show(ex.ToString()); }
     finally { StartWatch(); }
@@ -122,7 +122,7 @@ public partial class TSMainWindow : Window
       _ctsAudio?.Cancel();
       await _bpr.TickAsync();
       brdr1.Background = Brushes.Cyan;
-      Title = $"{VersionHelper.CurVerStr0Md}  -  {DateTime.Now:HH:mm:ss} Moved Olds   ";
+      Title = $"{DateTime.Now:HH:mm}  Olds moved  ..  {VersionHelper.CurVerStr0Md}  ";
     }
     catch (Exception ex) { _ = MessageBox.Show(ex.ToString()); }
     finally { StartWatch(); }
@@ -140,9 +140,10 @@ public partial class TSMainWindow : Window
       WindowState = WindowState.Minimized;
 
     brdr1.Background = Brushes.DarkCyan;
-    await Task.Delay(_200ms * 8); Title = $"{VersionHelper.CurVerStr0Md}  -  {DateTime.Now:HH:mm:ss} minimized  * * * ";
+    //?await Task.Delay(_200ms * 8); 
     Topmost = false;
     await _bpr.TickAsync();
+    Title = $"{DateTime.Now:HH:mm}  Minimized  ..  {VersionHelper.CurVerStr0Md}  ";
   }
 
   void On0000(object s, RoutedEventArgs e) { _bpr.Click(); try { } catch (Exception ex) { _ = MessageBox.Show(ex.ToString()); } }
@@ -304,7 +305,7 @@ public partial class TSMainWindow : Window
       }
     }
 
-    Title = $"▄▀▄▀▄▀▄▀   {VersionHelper.CurVerStr0Md}";
+    Title = $"{DateTime.Now:HH:mm}  ▄▀▄▀▄▀▄▀   {VersionHelper.CurVerStr0Md}";
 
     _ = ReScanFolder_SetCurrentStateToWatchChangesAgainst(tbxPath.Text); // resets the mark to prevent the changes to be picked by the FS checker and re-start the alarm.
   }
@@ -372,7 +373,7 @@ public partial class TSMainWindow : Window
 
       _ = Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
       {
-        Title = $"Out";
+        Title = $"{DateTime.Now:HH:mm}  Out";
         tbkHeadr.Text = $" {_w} {--_v} {_a} ";
       }));
     }
@@ -406,7 +407,7 @@ public partial class TSMainWindow : Window
     WriteLine($"{DateTime.Now:HH:mm:ss}  OnStateChanged({WindowState})");
     switch (WindowState)
     {
-      default:
+      default: break;
       case WindowState.Minimized: await AckAck(s); break;
     }
   }
