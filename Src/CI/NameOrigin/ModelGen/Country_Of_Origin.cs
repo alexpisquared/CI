0bfb7ded6ebf721f5af8eb87a50da390f9f19d05
AlexPi <alex.pigida@outlook.com>
2022-10-30 17:05:33 -0400

    pau

---
+++ Src/CI/NameOrigin/ModelGen/Country_Of_Origin.cs
@@ -0,0 +1,10 @@
+﻿namespace NameOrigin.ModelGen;
+
+public class Country_Of_Origin
+{
+  public string country_name { get; set; }
+  public string country { get; set; }
+  public float probability { get; set; }
+  public string continental_region { get; set; }
+  public string statistical_region { get; set; }
+}
---
+++ Src/CI/NameOrigin/ModelGen/NameRootObject.cs
@@ -0,0 +1,14 @@
+﻿namespace NameOrigin.ModelGen;
+
+public class NameRootObject
+{
+  public string name { get; set; }
+  public Country_Of_Origin[] country_of_origin { get; set; }
+  public string name_sanitized { get; set; }
+  public string gender { get; set; }
+  public int samples { get; set; }
+  public int accuracy { get; set; }
+  public string country_of_origin_map_url { get; set; }
+  public int credits_used { get; set; }
+  public string duration { get; set; }
+}
