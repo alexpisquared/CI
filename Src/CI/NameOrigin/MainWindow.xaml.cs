91030e8b954eabf530c3bb50b5771f454f88d40e
AlexPi <alex.pigida@outlook.com>
2022-11-03 11:35:36 -0400

    pau

--- Src/CI/NameOrigin/MainWindow.xaml.cs
+++ Src/CI/NameOrigin/MainWindow.xaml.cs
@@ -1,20 +1,20 @@
-﻿using System.Windows;
-using GenderApiLib;
+﻿using GenderApiLib;
 using NameOrigin.Code1stModelGen;
 namespace NameOrigin;
 public partial class MainWindow : Window
 {
   public MainWindow() => InitializeComponent();
 
+  void OnLoaded(object sender, RoutedEventArgs e) => tbNm.Focus();
+  void OnClose(object sender, RoutedEventArgs e) => Close();
   void OnCreateDB(object sender, RoutedEventArgs e) => DbCreator.Create();
   void OnReadJson(object sender, RoutedEventArgs e) => JsonReadr.ReadArray();
   async void OnGenderApi(object sender, RoutedEventArgs e)
   {
-    var key = new ConfigurationBuilder().AddUserSecrets<App>().Build()["AppSecrets:MagicNumber"]; //tu: adhoc usersecrets from config
+    _ = new ConfigurationBuilder().AddUserSecrets<App>().Build()["AppSecrets:MagicNumber"]; //tu: adhoc usersecrets from config
 
-    var rv = await GenderApi.CallOpenAI(new ConfigurationBuilder().AddUserSecrets<App>().Build(), "Mina");
+    var (ts, _, root) = await GenderApi.CallOpenAI(new ConfigurationBuilder().AddUserSecrets<App>().Build(), tbNm.Text);
 
+    tbRv.Text = $"{root?.name_sanitized}   {root?.country_of_origin.FirstOrDefault()?.country_name}   {root?.country_of_origin.FirstOrDefault()?.probability} %    {ts.TotalMilliseconds:N0} ms";
   }
-
-  void OnClose(object sender, RoutedEventArgs e) => Close();
 }
