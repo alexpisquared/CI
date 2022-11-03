f073454efecb8618f31872cf22a583959b809768
AlexPi <alex.pigida@outlook.com>
2022-11-03 09:04:10 -0400

    caching

--- Src/CI/NameOrigin/GenderApi.cs
+++ Src/CI/NameOrigin/GenderApi.cs
@@ -1,41 +1,49 @@
-﻿using NameOrigin.Code1stModelGen;
+﻿using System.Security.Policy;
+using NameOrigin.Code1stModelGen;
 
 namespace NameOrigin;
 public class GenderApi
 {
   public static async Task<(TimeSpan ts, string finishReason, FirstnameRootObject? root)> CallOpenAI(IConfigurationRoot cfg, string firstName)
   {
-    var sw = Stopwatch.StartNew();
-    var key = cfg?["GenderApi"] + "6c7ef4601";
-    var url = $"https://gender-api.com/get-country-of-origin?name={firstName}&key={key}";
+    var stopwatch = Stopwatch.StartNew();
+    var filename = $@"C:\g\CI\Src\CI\NameOrigin\GenderApiResults\FirstName-{firstName}.json";
 
     try
     {
-      using var httpClient = new HttpClient();
-      using var requestMsg = new HttpRequestMessage(new HttpMethod("POST"), url);
+      var jsonPart = File.Exists(filename) ? await File.ReadAllTextAsync(filename) : await GetFromWeb(cfg, firstName);
+      if (jsonPart is null)
+        return (stopwatch.Elapsed, "jsonPart is null", null);
 
-      var response = await httpClient.SendAsync(requestMsg);//.Result;
-      var jsonPart = await response.Content.ReadAsStringAsync();//.Result;
-
-      if(jsonPart is null) 
-        return (sw.Elapsed, "jsonPart is null", null);
-
-      await File.WriteAllTextAsync($@"C:\g\CI\Src\CI\NameOrigin\GenderApiResults\FirstName-{firstName}-{DateTime.Now.Second}.json", jsonPart);
+      await File.WriteAllTextAsync(filename, jsonPart);
 
       var dynOb2 = JsonConvert.DeserializeObject<FirstnameRootObject>(jsonPart);          //dynamic dynObj = JsonConvert.DeserializeObject(jsonPart) ?? "No way!";          //dynamic dynObj = JsonSerializer.Deserialize(json);
 
       try
       {
-        return (sw.Elapsed, "", dynOb2);
+        return (stopwatch.Elapsed, "", dynOb2);
       }
       catch (Exception ex)
       {
-        return (sw.Elapsed, ex.Message, null);
+        return (stopwatch.Elapsed, ex.Message, null);
       }
     }
     catch (Exception ex)
     {
-      return (sw.Elapsed, ex.Message, null);
+      return (stopwatch.Elapsed, ex.Message, null);
     }
+  }
+
+  private static async Task<string> GetFromWeb(IConfigurationRoot cfg, string firstName)
+  {
+    var key = cfg?["GenderApi"] + "6c7ef4601";
+    var url = $"https://gender-api.com/get-country-of-origin?name={firstName}&key={key}";
+
+    using var httpClient = new HttpClient();
+    using var requestMsg = new HttpRequestMessage(new HttpMethod("POST"), url);
+
+    var response = await httpClient.SendAsync(requestMsg);//.Result;
+    var jsonPart = await response.Content.ReadAsStringAsync();//.Result;
+    return jsonPart;
   }
 }
